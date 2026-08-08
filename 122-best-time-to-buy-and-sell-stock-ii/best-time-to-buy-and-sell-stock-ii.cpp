1d6884b07d98e2c15acea7538af1ed99f8e7d166
Sahil Shaikh <158192974+SahilShaikh81048@users.noreply.github.com>
2026-08-09 01:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (66.75%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+
+        for(int i=0;i<prices.size();i++){
+            if(i<prices.size()-1){
+                if(prices[i+1]>prices[i]){
+                    profit+=(prices[i+1]-prices[i]);
+                }
+            }
+        }
+        return profit;
+    }
+};
